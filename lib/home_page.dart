be80991b14054ed73fc88a93a47355bd2c1969b3
brdayauon <brdayauon@gmail,com>
2020-06-23 15:57:47 -0700

    sorta fixed bottomnav bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterappflorafy/plant_profile.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,72 +9,169 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  _onTap(int index) {
+   // if (index == 1) {
+      Navigator.of(context)
+          .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+        return new PlantProfilePage();
+      }));
+//    }
+//    else if (index == 2){
+//      Navigator.of(context)
+//          .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+//        return new SignUpPage();
+//      }));
+//      else{
+//
+//    }
+//    }
+  }
+
   var plantProfileList = [];
+  //static PlantProfile plantProfile = plantProfile1;
 
   _HomePageState() {
     //load all plant profile from firebase database and
     // display in ListView
-    FirebaseDatabase.instance.reference().child("plants").once()
+    FirebaseDatabase.instance
+        .reference()
+        .child("plants")
+        .once()
         .then((datasnapshot) {
-          print("Successfully loaded the data");
-          print(datasnapshot);
-          print("Key:");
-          print(datasnapshot.key);
-          print("Value: ");
-          print(datasnapshot.value);
-          print("iterating the value map:");
-          var plantTmpList = [];
-          datasnapshot.value.forEach((k,v) {
-            print(k);
-            print(v);
-            plantTmpList.add(v);
-          });
-          print("Final Friend List: ");
-          print(plantTmpList);
-          plantProfileList = plantTmpList;
-          setState(() {
-
-          });
-
+      print("Successfully loaded the data");
+      print(datasnapshot);
+      print("Key:");
+      print(datasnapshot.key);
+      print("Value: ");
+      print(datasnapshot.value);
+      print("iterating the value map:");
+      var plantTmpList = [];
+      datasnapshot.value.forEach((k, v) {
+        print(k);
+        print(v);
+        plantTmpList.add(v);
+      });
+      print("Final Plant List: ");
+      print(plantTmpList);
+      plantProfileList = plantTmpList;
+      setState(() {});
     }).catchError((error) {
       print("Failed to load the data!");
       print(error);
     });
   }
-  
-
-
+//
+//  Widget getPlantProfile(
+//      BuildContext context, PlantProfile profile, int index) {
+//    return Container(
+//        child: Column(
+//      crossAxisAlignment: CrossAxisAlignment.start,
+//      children: <Widget>[
+//        Container(
+//          margin: EdgeInsets.all(5),
+//          child: Row(
+//            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//            children: <Widget>[
+//              Row(
+//                //profile picture. username  ROW.
+//                children: <Widget>[
+//                  Container(
+//                    margin: EdgeInsets.only(right: 10),
+////                        child: CircleAvatar(backgroundImage: NetworkImage(plantProfileList['image'] == null ? 'https://www.clipartmax.com/png/middle/171-1717870_stockvader-predicted-cron-for-may-user-profile-icon-png.png' : plantProfileList['image']),
+////                      ),
+//                    //  Text(PlantProfile.user.username,)
+//                  )
+//                ],
+//              ),
+//              IconButton(
+//                icon: Icon(Icons.more_horiz),
+//                onPressed: () {},
+//              )
+//            ],
+//          ),
+//        ),
+//        Container(
+//          constraints: BoxConstraints.expand(height: 1),
+//          color: Colors.grey,
+//        ),
+//        Container(
+//          constraints: BoxConstraints(maxHeight: 282),
+//          decoration:
+//              BoxDecoration(image: DecorationImage(image: profile.image)),
+//        ),
+//        Row(
+//          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//          children: <Widget>[],
+//        ),
+//      ],
+//    ));
+//  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      body: PageView(
-        children: <Widget>[
-          Container(
-            color: Colors.teal,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-        ],
-      ),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+              itemCount: plantProfileList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 60,
+                  child: Center(
+                    child: Image(
+                      image:
+                          NetworkImage('${plantProfileList[index]['image']}'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }),
+        )
+      ]),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle),
-            title: Text('Add Plant Profile'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.view_list),
-            title: Text('View Plant Profile'),
-          ),
-        ],
+        items: _items,
+
+        onTap: _onTap,
       ),
     );
   }
+  final _items = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      title: Text('Home'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.add_circle),
+      title: Text('Add Plant Profile'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.view_list),
+      title: Text('View Plant Profile'),
+    ),
+  ];
+
 }
+
+
+
+
+//class PlantProfile {
+//  //each post has a picture and description.
+//  // So each picture has picture description of plant
+//  //
+//  AssetImage image;
+//  String description;
+//  User user;
+//
+//  PlantProfile(this.image, this.user, this.description);
+//}
+
+
+//class User {
+//  //name of user
+//  // profile picture
+//  String username;
+//  AssetImage profilePicture;
+//  User(this.username, this.profilePicture);
+//}
